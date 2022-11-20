d3033accf20905d5bccc82249ae5386e4534be50
Manan-2001 <jainmanan03052001@gmail.com>
2022-11-20 09:43:31 +0530

    nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { TiffinComponent } from './tiffin/tiffin.component';
 import { VehicleComponent } from './vehicle/vehicle.component';
 import { AccomodationComponent } from './accomodation/accomodation.component';
 import { AppliancesComponent } from './appliances/appliances.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-=======
 import { TifComponent } from './tif/tif.component';
->>>>>>> c997639813938656ce5dc1ba75f27ee86fa1bcaf
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     TiffinComponent,
     VehicleComponent,
     AccomodationComponent,
     AppliancesComponent,
     NavbarComponent,
-    FooterComponent
-=======
+    FooterComponent,
     TifComponent
->>>>>>> c997639813938656ce5dc1ba75f27ee86fa1bcaf
   ],
   imports: [
     BrowserModule,
