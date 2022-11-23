7b57815e452b9b8e9f33b61ea362a07f3e242f55
nikhilkawdeti <nikhilkawdeti.learn@gmail.com>
2022-11-23 15:12:33 +0530

    faq comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,8 @@ import { PrevApplianceButtonDirective } from './appliances/buttons/for Appliance
 import { PrevFurnitureButtonDirective } from './appliances/buttons/for furniture list/prev-furniture-button.directive';
 import { NextApplianceButtonDirective } from './appliances/buttons/for Appliance list/next-appliance-button.directive';
 import { NextFurnitureButtonDirective } from './appliances/buttons/for furniture list/next-furniture-button.directive';
-<<<<<<< HEAD
 import { HomeComponent } from './home/home.component';
-=======
-import { LoginComponent } from './login/login.component';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
->>>>>>> ca2b5e69c30a83a90181ddc235167384691e5722
-
+import { FaqPageComponent } from './faq-page/faq-page.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,13 +32,8 @@ import { ContactComponent } from './contact/contact.component';
     PrevFurnitureButtonDirective,
     NextApplianceButtonDirective,
     NextFurnitureButtonDirective,
-<<<<<<< HEAD
-    HomeComponent
-=======
-    LoginComponent,
-    AboutComponent,
-    ContactComponent
->>>>>>> ca2b5e69c30a83a90181ddc235167384691e5722
+    HomeComponent,
+    FaqPageComponent
   ],
   imports: [
     BrowserModule,
