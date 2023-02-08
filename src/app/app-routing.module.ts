291ff520f6c1ab6483819c2f33606e8f07ffb47a
Vijaya <vijayapaunikar2001@gmail.com>
2023-02-08 20:21:35 +0530

    UI Routerlink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,12 +49,13 @@ const routes: Routes = [
     path: "tiffin",
     component: TiffinComponent
   },
+  
   {
-    path: "home",
+    path: '',
     component: HomeComponent
   },
   {
-    path: "",
+    path: "home",
     component: HomeComponent
   },
 
